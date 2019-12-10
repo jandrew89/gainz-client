d672a2bfe501c873a4bbfcd9ed7e2ceb1e1ae1de
Jason Christman <jasonandrewchristman@gmail.com>
2019-12-09 21:49:19 -0700

    added equipment service call

--- src/app/components/lifts/equipment-detail/equipment-detail.component.ts
+++ src/app/components/lifts/equipment-detail/equipment-detail.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnChanges } from '@angular/core';
 import { Equipment } from 'src/app/data/entities/equipment';
 import { SessionType } from 'src/app/data/entities/session';
+import { EquipmentService } from 'src/app/data/services/equipment.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equipment-detail',
   templateUrl: './equipment-detail.component.html',
   styleUrls: ['./equipment-detail.component.css']
 })
-export class EquipmentDetailComponent implements OnInit {
+export class EquipmentDetailComponent implements OnInit, OnChanges {
 
   @Input() displayEquipmentModal: boolean
   @Input() equipment: Equipment;
-  @Input() sessionTypes: SessionType[];
   @Output() closeEquipmentEdit = new EventEmitter();
 
-  constructor() { }
+  equipmentForm = new FormGroup({
+    name: new FormControl(''),
+    sessionTypes: new FormControl('')
+  });
+  
+  sessionTypes: SessionType[];
+
+  constructor(private equipmentService: EquipmentService) { }
 
 
   ngOnInit() {
+    this.equipmentService.getSessionTypes().subscribe(
+      sessionTypes =>  { 
+        this.sessionTypes = sessionTypes;
+      }
+    )
+  }
+
+  ngOnChanges(changes: any): void {
+      this.equipmentForm.patchValue({
+        name: this.equipment.name,
+        sessionTypes: this.equipment.sessionTypes
+      });
   }
 
 
@@ -25,7 +45,36 @@ export class EquipmentDetailComponent implements OnInit {
     this.closeEquipmentEdit.emit(false);
   }
 
-  saveChanges(): void {
+  saveEquipmentForm(): void {
+    var name = this.equipmentForm.controls['name'].value
+    var types = this.equipmentForm.controls['sessionTypes'].value as []
     
+    if (name === null || types === null) {
+      //TODO: Validation error
+      return
+    }
+    var sessionTypesToUpdate = this.sessionTypes.filter(type => types.some(s => s == type.id));
+  
+    //save updates
+    this.equipmentService.updateEquipment({
+      id: this.equipment.id,
+      name: name,
+      sessionTypes: sessionTypesToUpdate
+    }).subscribe(
+      isSuccesful => {
+        if (isSuccesful){
+          //Toastr success message
+        }
+      }
+    )
+
+    this.closeEquipmentEdit.emit(false);
+  }
+
+  isTypeSelected(id: string) {
+    if (!this.equipment.sessionTypes) {
+      return false;
+    }
+    return this.equipment.sessionTypes.some(st => st.id === id);
   }
 }
