6275945d6fd6f81c2c9a5fe3cb978eb5cb02ad6f
Jason Christman <jasonandrewchristman@gmail.com>
2019-12-19 19:51:10 -0700

    added menu sandwich on mobile

--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,8 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.sidenav').sidenav();
+    });
   }
-
 }
