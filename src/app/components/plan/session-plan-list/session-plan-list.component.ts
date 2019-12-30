2db17aa801b23f6fca05e1db2af080e0417bb779
Jason Christman <jasonandrewchristman@gmail.com>
2019-12-30 11:04:03 -0700

    added tabs

--- src/app/components/plan/session-plan-list/session-plan-list.component.ts
+++ src/app/components/plan/session-plan-list/session-plan-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-session-plan-list',
@@ -10,6 +11,9 @@ export class SessionPlanListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('ul.tabs').tabs();
+    });
   }
 
 }
