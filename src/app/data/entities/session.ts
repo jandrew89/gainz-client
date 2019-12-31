4d0bd6fe1eae728726ec51a3a1b42fe5f24591ca
Jason Christman <jasonandrewchristman@gmail.com>
2019-12-30 22:16:46 -0700

    added session plan view model

--- src/app/data/entities/session.ts
+++ src/app/data/entities/session.ts
@@ -5,10 +5,16 @@ export interface Session {
     weight?: number;
     sessionDate?: Date;
     sessionType: string;
+    sessionPlan?: SessionPlanViewModel
     activities: Activity[];
 }
 
 export interface SessionType {
     id: string;
     name: string;
+}
+
+export interface SessionPlanViewModel {
+    id: string;
+    sessionPlanName: string;
 }
