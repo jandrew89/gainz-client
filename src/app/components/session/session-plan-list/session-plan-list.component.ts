8058150fc97ad6cb0e65b35470004b4a38fdb66d
Jason Christman <jasonandrewchristman@gmail.com>
2019-12-31 15:14:13 -0700

    added new session plan list modal comp

---
+++ src/app/components/session/session-plan-list/session-plan-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-plan-list',
+  templateUrl: './session-plan-list.component.html',
+  styleUrls: ['./session-plan-list.component.css']
+})
+export class SessionPlanListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
