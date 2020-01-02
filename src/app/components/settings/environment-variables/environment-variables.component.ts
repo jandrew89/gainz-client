707ee0bcbe3680ebe29876245fdd8795ba39bc27
Jason Christman <jasonandrewchristman@gmail.com>
2020-01-01 18:45:31 -0700

    added env var comp

---
+++ src/app/components/settings/environment-variables/environment-variables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-environment-variables',
+  templateUrl: './environment-variables.component.html',
+  styleUrls: ['./environment-variables.component.css']
+})
+export class EnvironmentVariablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
