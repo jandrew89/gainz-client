38e6b5debb2f92f4fad8590456d5158371c8762c
Jason Christman <jasonandrewchristman@gmail.com>
2020-01-07 17:49:40 -0700

    session plan edit modal infrasture

--- src/app/components/settings/session-plan-edit-settings/session-plan-edit-settings.component.ts
+++ src/app/components/settings/session-plan-edit-settings/session-plan-edit-settings.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { SessionPlan } from 'src/app/data/entities/session-plan';
+import { FormGroup, FormControl } from '@angular/forms';
+declare var $: any;
 @Component({
   selector: 'app-session-plan-edit-settings',
   templateUrl: './session-plan-edit-settings.component.html',
   styleUrls: ['./session-plan-edit-settings.component.css']
 })
-export class SessionPlanEditSettingsComponent implements OnInit {
+export class SessionPlanEditSettingsComponent implements OnInit, OnChanges {
+  @Input() displaySessionPlanModal: boolean
+  @Input() sessionPlan: SessionPlan
+  @Output() closePlanEdit = new EventEmitter();
+
+  sessionPlanForm = new FormGroup({
+    sessionPlanName: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.modal').modal();
+    });
+
+    this.sessionPlanForm.patchValue({
+      sessionPlanName: this.sessionPlan.sessionPlanName
+    });
   }
 
+  ngOnChanges(changes: any): void {
+    this.sessionPlanForm.patchValue({
+      sessionPlanName: this.sessionPlan.sessionPlanName
+    });
+  }
 }
