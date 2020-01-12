3f893230d4a4ddfe2641253f9fd615e576f2b588
Jason Christman <jasonandrewchristman@gmail.com>
2020-01-11 19:40:08 -0700

    added subject

--- src/app/data/services/auth.service.ts
+++ src/app/data/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { UserManager, User } from 'oidc-client';
 import { environment } from 'src/environments/environment';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,10 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
   private _userManager: UserManager;
   private _user: User;
-  
+  private _loginChangedSubject = new Subject<boolean>();
+
+  loginChanged = this._loginChangedSubject.asObservable();
+
   constructor() { 
     
     const stsSettings = {
@@ -21,5 +25,22 @@ export class AuthService {
     };
 
     this._userManager = new UserManager(stsSettings);
+  }
+
+  login() {
+    return this._userManager.signinRedirect();
+  }
+
+  isLogginIn(): Promise<boolean> {
+    return this._userManager.getUser().then(
+      user => {
+        const userCurrent = !!user && !user.expired;
+        if (this._user !== user){
+          this._loginChangedSubject.next(userCurrent);
+        }
+        this._user = user;
+        return userCurrent;
+      }
+    )
   }
 }
